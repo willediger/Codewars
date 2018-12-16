184b658bb3e9969e079a0602605696fe119df349
will Ediger <will.ediger@gmail.com>
2018-12-15 21:04:07 -0600

    added another test case, and split up single line into multiple lines for readability

--- findEvenIndex.js
+++ findEvenIndex.js
@@ -1,5 +1,9 @@
 function findEvenIndex(arr) {
-  return arr.map((val, i) => arr.reduce((acc, val, j) => i < j ? acc + val : acc, 0) == arr.reduce((acc, val, j) => i > j ? acc + val : acc, 0)).indexOf(true);
+  return arr.map((val, i) => 
+    arr.reduce((acc, val, j) => i < j ? acc + val : acc, 0) == 
+    arr.reduce((acc, val, j) => i > j ? acc + val : acc, 0)
+  ).indexOf(true);
 }
 
-console.log(findEvenIndex([1,2,3,4,5,6]))
+console.log(findEvenIndex([1,2,3,4,5,6]));
+console.log(findEvenIndex([1,2,3,4,3,2,1]));
